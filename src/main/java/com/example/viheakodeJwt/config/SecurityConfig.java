50ae96ffb177c9b067a31bf1d177d0fd52de1bd2
viheakode <viheakode787@gmail.com>
2025-12-27 18:20:21 +0700

    Working on create user

--- src/main/java/com/example/viheakodeJwt/config/SecurityConfig.java
+++ src/main/java/com/example/viheakodeJwt/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/", "/login", "/api/v1/user/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
