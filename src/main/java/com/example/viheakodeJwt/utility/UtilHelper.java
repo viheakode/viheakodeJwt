258bb65a096641bdbadc127bf892be78e8b9cf02
viheakode <viheakode787@gmail.com>
2025-12-27 17:40:01 +0700

    config db

--- src/main/java/com/example/viheakodeJwt/utility/UtilHelper.java
+++ src/main/java/com/example/viheakodeJwt/utility/UtilHelper.java
@@ -1,4 +1,4 @@
-package com.example.viheakodeJwt.util;
+package com.example.viheakodeJwt.utility;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
