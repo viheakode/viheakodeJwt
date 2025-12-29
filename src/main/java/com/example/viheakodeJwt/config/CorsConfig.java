8a3d5c4cc238d593733d36cdfba1504986584ab6
viheakode <viheakode787@gmail.com>
2025-12-29 09:22:23 +0700

    config properties file

--- src/main/java/com/example/viheakodeJwt/config/CorsConfig.java
+++ src/main/java/com/example/viheakodeJwt/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5000")
+                .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
